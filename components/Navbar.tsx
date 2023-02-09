1cf12f454ce0de77967cd1f9867465eff9e54489
clint911 <clingyking007@gmail.com>
2023-02-09 13:44:32 +0300

    changing navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch  } from 'react-router-dom';
-import Home from './Home';
-import Shop from './Shop';
-import About from './About';
-import Contact from './Contact';
+import React from "react";
 
-const Navbar: React.FC = () => (
-  <nav>
-    <ul>
-      <li>
-        <BrowserRouter> 
-        <Switch> 
-        <Route path="/components/Home.tsx">
-          <Home /> 
-        </Route>
-      <li>
-        <Route path="/components/Shop.tsx">
-          <Shop /> 
-        </Route>
-      </li>
-      <li>
-        <Route path='/components/About.tsx'>
-          <About /> 
-        </Route>
-      </li>
-      <li>
-        <Route path='/components/Contact.tsx'>
-          <Contact /> 
-          </Route>
-                    </li>
-              </Switch>
-        </BrowserRouter>
-      </li>
-    </ul>
-  </nav>
-);
-
-export default Navbar;
+export default function NavBar() {
+  return (
+    <div className="NavBar">
+      <h1>Navbar will be added here </h1>
+    </div>
+  )
+}
